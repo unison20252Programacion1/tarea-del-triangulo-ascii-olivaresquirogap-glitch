3db15a66237794e04e8e5daf7ea4d90e4003b8fe
olivaresquirogap-glitch <olivaresquirogap@gmail.com>
2025-11-21 14:03:40 -0700

    Update solucion.py

--- solucion.py
+++ solucion.py
@@ -2,5 +2,13 @@
 
 def triangulo_simetrico(m: int, s: str) -> str:
     # TODO: validar altura mayor que 0 e imprimir "Error: La altura debe ser un entero positivo" y salir
-    
+    if m <= 0:
+        print("Error: La altura debe ser un entero positivo")
+        return
+
+    for i in range(1, m + 1):
+        print(s * i)
+
+    for i in range(m - 1, 0, -1):
+        print(s * i)
     # TODO: implementar la lógica para generar el triángulo simétrico en ASCII
